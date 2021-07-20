d8adc5668dd18f0171f832a945531d606dbb295e
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-07-20 12:21:10 -0700

    added array that houses the question, options, and correct answer for each question

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        title: "True or False, JavaScript and Java are the same",
+        options: ['True', 'False'],
+        answer: 'False'
+
+    },
+    {
+        title: "An array must be placed in between ______",
+        options: ['quotes', 'curly brackets','parentheses', 'square brackets'],
+        answer: 'square brackets'
+
+    },
+    {
+        title: "Which of the following correctly calls the function, \"myFunction\"",
+        options: ['call myFunction()', 'call function myFunction()', 'myFunction()', 'myFunction'],
+        answer: 'myFunction()'
+
+    },
+    {
+        title: "This method will allow a user to input information, which will be stored as a string",
+        options: ['prompt()', 'alert()', 'confirm()', 'input()'],
+        answer: ""
+
+    },
+    {
+        title: "Which of the following is a correctly formatted \"FOR\" loop",
+        options: ['for (i = 0; i <=5)', 'for (i <= 5; i++', 'for (i = 0; i <= 5; i++)', 'for i = 1 to 5'],
+        answer: 'for (i = 0; i <= 5; i++)'
+
+    }
+];
