18a64f0ed99082bfc13063c3d09df411cf5c5db8
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-09 02:32:40 -0700

    added comments throughout js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+//DOM element variables
 const questionsEl = document.querySelector('#questions');
 const titleEl = document.querySelector('#title');
 const optionsEl = document.querySelector('#options');
@@ -9,27 +10,32 @@ const initialsEl = document.querySelector('#initials');
 const scoreBtn = document.querySelector('#submit');
 const startEl = document.querySelector('.start');
 
+//Quiz status variables
 let currentQuestion = 0;
 let timeRemain = 75;
 let timerStart
 
 function startQuiz() {
-
+    //hides start screen
     startEl.setAttribute('class', 'hide');
-
+    
+    //displays questions screen
     questionsEl.removeAttribute('class', 'hide')
-
+    
+    //sets timer display to starting time
     timeEl.textContent = timeRemain;
-
+    //starts timer
     timerStart = setInterval(timeTick, 1000);
 
     createQuestion();
 }
 
 function timeTick() {
+    //ticks remaining time down and updates display to reflect change
     timeRemain--
     timeEl.textContent = timeRemain
     
+    //runs endQuiz function if time runs out
     if (timeRemain <= 0) {
         console.log('Game Over!');
         endQuiz();
@@ -37,28 +43,38 @@ function timeTick() {
 };
 
 function createQuestion() {
+    //sets question of question element based on current index of questions.js
     titleEl.textContent = questions[currentQuestion].title
-
+    
+    //clears any previous question options
     optionsEl.innerHTML = '';
 
     questions[currentQuestion].options.forEach(function (option) {
+        //creates new button per available option
+        //sets text of new button to match corresponding option
         let optionButton = document.createElement('button');
         optionButton.textContent = option;
 
+        //apprends created button el to existing element on page
         optionsEl.appendChild(optionButton);
 
+        //ties event listener to each button
         optionButton.addEventListener('click', checkAnswer);
     });
 };
 
 function checkAnswer(event) {
     const corrAnswer = questions[currentQuestion].answer
+    //compares textContent of clicked element with correct answer listed in the corresponding object property
+    //removes 10 seconds from timer if answer is incorrect
     if (event.target.textContent != corrAnswer) {
     timeRemain -= 10
     };
-
+    
+    //advances index of questions.js array
     currentQuestion++
 
+    //runs endQuiz function if end of array is reached, otherwise runs createQuestion function
     if (currentQuestion === questions.length) {
     console.log('Game Over!');
     endQuiz();
@@ -68,31 +84,42 @@ function checkAnswer(event) {
 };
 
 function endQuiz() {
+    //stops timer
     clearInterval(timerStart);
-
+    
+    //hides questions screen
     questionsEl.setAttribute('class', 'hide');
 
+    //displays end screen
     endEl.removeAttribute('class', 'hide');
 
+    //sets current time remaining as final score
     finalScoreEl.textContent = timeRemain;
 };
 
-function logScore(params) {
+function logScore() {
+    //captures user input
     const initials = initialsEl.value;
-
+    
+    //sets newScore object with current users information
     const newScore = {
         initials: initials,
         score: timeRemain
     };
-
+    
+    //pulls scores from localStorage or sets an empty array
     const allScores = JSON.parse(localStorage.getItem('allScores')) || [];
 
+    //saves newly created score object into localStorage array
     allScores.push(newScore);
-
     localStorage.setItem('allScores', JSON.stringify(allScores));
 
+    //redirects window to scoreboard page
     window.location.href = 'highscores.html'
 };
 
+//ties quiz start function to correct button el
 startBtn.onclick = startQuiz;
+
+//ties score save function to correct button el
 scoreBtn.onclick = logScore;
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,24 +1,33 @@
+
 function displayScores() {
+    //pulls scores from localStorage or sets an empty array
     const allScores = JSON.parse(localStorage.getItem('allScores')) || [];
 
+    //sorts scores in decending order
     allScores.sort(function (a, b) {
         return b.score - a.score;
     });
 
     allScores.forEach(function (score) {
+        //creates an li el per saved score
+        //sets textContent of li tag to saved initials/score for each index of saved array
         const scoreEl = document.createElement('li');
         scoreEl.textContent = score.initials + ' - ' + score.score
-
+        
+        //appends created li el to existing element on page
         const scoreBoard = document.querySelector('#allScores');
         scoreBoard.appendChild(scoreEl);
     });
 }
 
 function clearScores() {
-    window.localStorage.removeItem('allScores');
+    //clears localStorage, removing all initials/scores
+    //reloads page to display change
+    localStorage.removeItem('allScores');
     window.location.reload();
 }
 
+//ties clear function to correct button el
 document.querySelector('#clear').onclick = clearScores;
 
 displayScores();
